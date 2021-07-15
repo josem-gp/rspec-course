31cbeea1c97397ddbbf34d74adfdab46ffafbd9e
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-15 18:25:38 +0900

    have_attribute matcher

---
+++ spec/have_attributes_matcher_spec.rb
@@ -0,0 +1,21 @@
+#checks whether an object has a set of attributes along with their values
+
+class ProfessionalWrestler
+    attr_reader :name, :finishing_move
+
+    def initialize(name, finishing_move)
+        @name = name
+        @finishing_move = finishing_move
+    end
+end
+
+RSpec.describe 'have_attributes matcher' do
+    describe ProfessionalWrestler.new('Stone Cold Steve Austin', 'Stunner') do
+        it 'checks for object attribute and proper value' do
+            expect(subject).to have_attributes(name: 'Stone Cold Steve Austin')
+            expect(subject).to have_attributes(name: 'Stone Cold Steve Austin', finishing_move: 'Stunner')
+        end
+        it { is_expected.to have_attributes(name: 'Stone Cold Steve Austin') }
+        it { is_expected.to have_attributes(name: 'Stone Cold Steve Austin', finishing_move: 'Stunner') }
+    end
+end
