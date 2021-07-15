ccee2fab829c3b32b3d030f021a06496fc804be5
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-15 14:31:30 +0900

    not_to matcher rspec

---
+++ spec/not_to_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe 'not_to method' do
+    it 'checks that two values do not match' do
+        expect(5).not_to eq(4)
+        expect('Hello').not_to eq('hello')
+        expect([1,2,3]).not_to eq([1,2])
+    end
+
+end
