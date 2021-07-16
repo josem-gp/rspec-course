2d4effa4b6bafa279e5805a9470cfd1646ece893
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-16 19:18:16 +0900

    raise_error matcher

--- spec/raise_error_matcher_spec.rb
+++ spec/raise_error_matcher_spec.rb
@@ -1,2 +1,26 @@
 #check that our program has an exception working correctly to handle an error
 
+RSpec.describe 'raise_error matcher' do
+
+    def some_method
+        x
+    end
+
+    class CustomError < StandardError; end #to customize the errors message
+
+    it 'can check for any error' do
+        expect { some_method }.to raise_error #since this one is too general, RSpec will warn us saying it is too broad and we risk catching errors we didnt mean to
+    end
+
+    it 'can check for a specific error' do
+        expect { some_method }.to raise_error(NameError)
+    end
+
+    it 'can check another specific error' do
+        expect { 10/0 }.to raise_error(ZeroDivisionError) 
+    end
+
+    it 'can check that a user-created error works properly' do
+        expect { raise CustomError }.to raise_error(CustomError)
+    end
+end
