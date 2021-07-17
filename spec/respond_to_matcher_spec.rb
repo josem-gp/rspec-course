796b1e3512f6051f58a3339568133b6ad491105a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-17 13:39:08 +0900

    respond_to matcher

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -14,8 +14,17 @@ class HotChocolate
     end
 end
 
-class Coffee
-    def drink; end
-    def discard; end
-    def purchase(number); end
+RSpec.describe HotChocolate do
+    it 'confirms that an object can respond to a method' do
+        expect(subject).to respond_to(:drink)
+        expect(subject).to respond_to(:drink, :discard)
+        expect(subject).to respond_to(:drink, :discard, :purchase)
+    end
+
+    it 'confirms that an object can respond to a method with arguments' do
+        expect(subject).to respond_to(:purchase).with(1).arguments #we put 1 since the method has one argument
+    end
+
+    it { is_expected.to respond_to(:purchase, :discard) }
+    it { is_expected.to respond_to(:purchase).with(1).arguments }
 end
