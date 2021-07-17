4de2e70e027b70e1ed8db87cbcf14d22e042d710
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-17 21:38:52 +0900

    compound matchers with and

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -2,9 +2,17 @@
 
 RSpec.describe 25 do
     it 'can test for multiple matchers' do
-        expect(subject).to be_odd and be > 20
+        expect(subject).to be_odd.and be > 20
     end
 
-    it { is_expected.to be_odd and be > 20 }
+    it { is_expected.to be_odd.and be > 20 }
 end
 
+
+RSpec.describe 'caterpillar' do
+    it 'supports multiple matchers on a single line' do
+        expect(subject).to start_with('cat').and end_with('pillar')
+    end
+
+    it { is_expected.to start_with('cat').and end_with('pillar') }
+end
