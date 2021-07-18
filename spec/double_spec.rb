7596d0f4c4468f9b2798665de14bfb6810d16bba
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-18 11:31:53 +0900

    second example double_spec

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -3,8 +3,13 @@
 
 RSpec.describe 'a random double' do
     it 'only allows defined methods to be invoked' do
-        stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch', light_on_fire: true)
+        # stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch', light_on_fire: true) #this lets us create a testable object with 2 methods and we just put what do we want those methods to return.
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+        # expect(stuntman.light_on_fire).to eq(true)
+        
+        stuntman = double("Mr. Danger")
+        allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
         expect(stuntman.fall_off_ladder).to eq('Ouch')
-        expect(stuntman.light_on_fire).to eq(true)
+    
     end
 end
