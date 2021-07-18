7fe64f6d805eda63fd92027f2de0ec0bafecaf5a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-18 10:18:18 +0900

    second exercises compound matchers

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -28,3 +28,9 @@ RSpec.describe 'firetruck' do
         expect(subject).to start_with('fire').and end_with('truck')
     end
 end
+
+RSpec.describe 20 do
+    it 'checks multiple matchers' do
+        expect(subject).to be_even.and respond_to(:times)
+    end
+end
