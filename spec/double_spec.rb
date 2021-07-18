cf3eed3a8dc9ff84e5b1eaa3f2d45496cbdb66ef
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-18 12:10:52 +0900

    assignment for double spec

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,4 +1,4 @@
-# creates a testable (objects that respond to any methods that we define on it).
+# creates a testable (objects that respond to any methods that we define on it). Used for unit testing.
 # we use these doubles to stand in for other real objects we are using in the program
 
 RSpec.describe 'a random double' do
@@ -11,9 +11,24 @@ RSpec.describe 'a random double' do
         # allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
         # expect(stuntman.fall_off_ladder).to eq('Ouch')
         
-        stuntman = double("Mr. Danger")
+        stuntman = double("Mr. Danger") #third way of doing it
         allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
         expect(stuntman.fall_off_ladder).to eq('Ouch')
         expect(stuntman.light_on_fire).to eq(true)
+    end
+end
+
+RSpec.describe 'doubles' do
+    it "tests the student's knowledge of the course's content" do
+        database = double("Database Connection", connect: true, disconnect: 'Goodbye')
+        expect(database.connect).to eq(true)
+        expect(database.disconnect).to eq('Goodbye')
+    end
+
+    it "tests the student's knowledge of the course's content" do
+        fs = double("File System")
+        allow(fs).to receive_messages(read: 'Romeo and Juliet', write: false)
+        expect(fs.read).to eq('Romeo and Juliet')
+        expect(fs.write).to eq(false)
     end
 end
